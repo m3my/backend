c925c3adc622ebb9515b5344c14a83767cada062
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2014-10-25 14:47:26 +0200

    basic skeleton implementation

---
+++ server.js
@@ -0,0 +1,13 @@
+//var api = require('./routes/api');
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/views'));
+//app.use('/api', api);
+
+//app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    console.log('Server listening on port ' + server.address().port);
+});
